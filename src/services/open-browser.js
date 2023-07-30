acfab8676ed05bf7901b5addc5a20f993c3c3c65
Prantik <prantiknoor@gmail.com>
2023-07-30 17:22:16 +0600

    puppeteer v20 added, other removed

--- src/services/open-browser.js
+++ src/services/open-browser.js
@@ -1,16 +1,15 @@
-const chromium = require("chrome-aws-lambda");
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 require('dotenv').config()
 
 const openBrowser = async () =>
     await puppeteer.launch({
         headless: true,
         ignoreHTTPSErrors: true,
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath:
-            process.env.CHROMIUM_EXECUTABLE_PATH ||
-            (await chromium.executablePath),
+        // args: chromium.args,
+        // defaultViewport: chromium.defaultViewport,
+        // executablePath:
+        //     process.env.CHROMIUM_EXECUTABLE_PATH ||
+        //     (await chromium.executablePath),
     });
 
 module.exports = openBrowser;
